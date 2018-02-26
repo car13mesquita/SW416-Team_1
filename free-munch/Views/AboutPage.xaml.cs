fe934cfd42a7c81e5dccca03e77f8d98e358b43b
nkwasnaza <32617681+nkwasnaza@users.noreply.github.com>
2018-02-25 19:02:41 -0500

    Initial File
    some may be duplicates

---
+++ free-munch/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace freemunch
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
