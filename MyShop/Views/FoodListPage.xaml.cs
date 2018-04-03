e71b0e5723ff8acbaad98a0883eeac5ab1856ced
Carlos Mesquita <car13mesquita@gmail.com>
2018-04-02 22:17:59 -0400

    Add multiselect list to foodlist page.

--- MyShop/Views/FoodListPage.xaml.cs
+++ MyShop/Views/FoodListPage.xaml.cs
@@ -12,5 +12,46 @@ namespace MyShop
 			InitializeComponent();
             BindingContext = new FoodListViewModel(this);
 		}
-	}
+
+        SelectMultipleBasePage<CheckItem> multiPage;
+        async void OnClick(object sender, EventArgs ea)
+        {
+            var items = new List<CheckItem>();
+            items.Add(new CheckItem { Name = "American" });
+            items.Add(new CheckItem { Name = "Chinese" });
+            items.Add(new CheckItem { Name = "Italian" });
+            items.Add(new CheckItem { Name = "Japanese" });
+            items.Add(new CheckItem { Name = "French" });
+            items.Add(new CheckItem { Name = "Greek" });
+            items.Add(new CheckItem { Name = "Thai" });
+            items.Add(new CheckItem { Name = "Spanish" });
+            items.Add(new CheckItem { Name = "Indian" });
+            items.Add(new CheckItem { Name = "Mediterranean" });
+            items.Add(new CheckItem { Name = "Mexican" });
+
+            if (multiPage == null)
+                multiPage = new SelectMultipleBasePage<CheckItem>(items) { Title = "Check all that apply" };
+
+            await Navigation.PushAsync(multiPage);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (multiPage != null)
+            {
+                results.Text = "";
+                var answers = multiPage.GetSelection();
+                foreach (var a in answers)
+                {
+                    results.Text += a.Name + ", ";
+                }
+            }
+            else
+            {
+                results.Text = "(none)";
+            }
+        }
+    }
 }
